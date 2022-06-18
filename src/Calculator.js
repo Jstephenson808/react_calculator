fac41bc9a0fbbf81a21946b7b98a6f183891a8c9
James Stephenson <2573758S@student.gla.ac.uk>
2022-06-18 13:16:36 +0100

    add input 2

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,15 +1,17 @@
 import {useState} from "react";
 
 export default function Calculator() {
-    const [inputValue, setInputValue] = useState('');
+    const [input1Value, setInput1Value] = useState('');
     const [outputValue, setOutputValue] = useState('');
     return (
         <>
-            <label htmlFor='input-field'>Input</label>
-                <input id='input-field' onChange={(event) => {setInputValue(event.target.value)}}/>
+            <label htmlFor='input-field-1'>Input 1</label>
+                <input id='input-field-1' onChange={(event) => {setInput1Value(event.target.value)}}/><br/>
+            <label htmlFor='input-field-2'>Input 2</label>
+                <input id='input-field-2'/><br/>
             <label htmlFor='output-field'>Output</label>
                 <input id='output-field' defaultValue={outputValue}/>
-            <button id='calculate' onClick={() => {setOutputValue(inputValue)}}>Calculate</button>
+            <button id='calculate' onClick={() => {setOutputValue(input1Value)}}>Calculate</button>
             <button id='+'>+</button>
             <button id='-'>-</button>
             <button id='x'>x</button>
--- src/Calculator.test.js
+++ src/Calculator.test.js
@@ -2,19 +2,26 @@ import {render, screen} from "@testing-library/react";
 import Calculator from "./Calculator";
 import userEvent from "@testing-library/user-event";
 
+const input1Field = () => screen.getByLabelText('Input 1');
+
+
+function calculateButton() {
+    return screen.getByText('Calculate');
+}
+
 describe('Calculator', () => {
     beforeEach(() => {
         // eslint-disable-next-line testing-library/no-render-in-setup
         render(<Calculator />);
     });
-    it('should have input field', () => {
-        expect(screen.getByLabelText('Input')).toBeInTheDocument();
+    it('should have input 1 field', () => {
+        expect(input1Field()).toBeInTheDocument();
     })
     it('should have output field', () => {
         expect(screen.getByLabelText('Output')).toBeInTheDocument();
     })
     it('should have a calculate button', () => {
-        expect(screen.getByText('Calculate')).toBeInTheDocument();
+        expect(calculateButton()).toBeInTheDocument();
     })
     it('should have a + button', () => {
         expect(screen.getByText('+')).toBeInTheDocument();
@@ -29,10 +36,13 @@ describe('Calculator', () => {
     it('should have x button', () => {
         expect(screen.getByText('x')).toBeInTheDocument();
     })
-    it('should display one when one is input and calculate is pressed', () => {
-        userEvent.type(screen.getByLabelText('Input'), '1');
-        userEvent.click(screen.getByText('Calculate'));
+    it('should display 1 when 1 is input1 and calculate is pressed', () => {
+        userEvent.type(input1Field(), '1');
+        userEvent.click(calculateButton());
 
         expect(screen.getByLabelText('Output')).toHaveValue('1');
+    })
+    it('should have an input 2 field', () => {
+        expect(screen.getByLabelText('Input 2')).toBeInTheDocument();
     })
 })

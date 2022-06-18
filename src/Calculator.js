d98c038d7e846fbac2f402f4c46142cded9f6d16
James Stephenson <2573758S@student.gla.ac.uk>
2022-06-18 12:19:39 +0100

    display input at output when Calculate is clicked

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,11 +1,15 @@
+import {useState} from "react";
+
 export default function Calculator() {
+    const [inputValue, setInputValue] = useState('');
+    const [outputValue, setOutputValue] = useState('');
     return (
         <>
             <label htmlFor='input-field'>Input</label>
-                <input id='input-field'/>
+                <input id='input-field' onChange={(event) => {setInputValue(event.target.value)}}/>
             <label htmlFor='output-field'>Output</label>
-                <input id='output-field'/>
-            <button id='calculate'>Calculate</button>
+                <input id='output-field' defaultValue={outputValue}/>
+            <button id='calculate' onClick={() => {setOutputValue(inputValue)}}>Calculate</button>
             <button id='+'>+</button>
             <button id='-'>-</button>
             <button id='x'>x</button>
--- src/Calculator.test.js
+++ src/Calculator.test.js
@@ -1,5 +1,6 @@
 import {render, screen} from "@testing-library/react";
 import Calculator from "./Calculator";
+import userEvent from "@testing-library/user-event";
 
 describe('Calculator', () => {
     beforeEach(() => {
@@ -9,7 +10,6 @@ describe('Calculator', () => {
     it('should have input field', () => {
         expect(screen.getByLabelText('Input')).toBeInTheDocument();
     })
-
     it('should have output field', () => {
         expect(screen.getByLabelText('Output')).toBeInTheDocument();
     })
@@ -28,5 +28,11 @@ describe('Calculator', () => {
     // Not sure about this one tbh, x could be found in lots of places
     it('should have x button', () => {
         expect(screen.getByText('x')).toBeInTheDocument();
+    })
+    it('should display one when one is input and calculate is pressed', () => {
+        userEvent.type(screen.getByLabelText('Input'), '1');
+        userEvent.click(screen.getByText('Calculate'));
+
+        expect(screen.getByLabelText('Output')).toHaveValue('1');
     })
 })
